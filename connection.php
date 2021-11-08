0ed159372f9b9dbc8697af4ce4f38dfdbedaf213
Giau64 <91658624+Giau64@users.noreply.github.com>
2021-11-08 12:14:31 +0700

    update pg

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php 
-    $conn = pg_connect('localhost', 'root', '', 'giau123')
+    $conn = pg_connect('localhost', 'root', '', 'PGASS')
             or die("Can not connect database".pg_connect_error());
 ?>
